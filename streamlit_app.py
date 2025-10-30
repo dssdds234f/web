47deb97d160590000c3c261fc4e14673f4a05256
dssdds234f <H098s21022@chunchon.gwe.hs.kr>
2025-10-30 15:10:56 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,13 +5,13 @@ import pandas as pd
 
 # --- 1. 페이지 기본 설정 ---
 st.set_page_config(
-    page_title="마법소녀 송지훈",
+    page_title="마법소녀",
     page_icon="🔮",
     layout="wide"
 )
 
 # --- 2. 페이지 타이틀 ---
-st.title("마법소녀 송지훈 🔮")
+st.title("마법소녀 🔮")
 st.subheader("HTML/CSS를 활용해 멋진 효과를 만들어 봐요!")
 st.markdown("---") # 구분선
 
